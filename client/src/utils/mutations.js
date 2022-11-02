b671bc64f9f675490830f3830a1d812d9cd800c3
bpiper91 <bpiper91@gmail.com>
2022-11-01 20:44:54 -0400

    added to utils/mutations.js, added concurrently and scripts to root folder

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,3 +12,38 @@ export const CREATE_USER = gql`
         }
     }
 `;
+
+export const CREATE_GROUP = gql`
+    mutation CreateGroup($groupName: String!, $description: String, $location: String) {
+        createGroup(groupName: $groupName, description: $description, location: $location) {
+            groupName
+            description
+            location
+            isPublic
+            listings {
+                _id
+                title
+                description
+                value
+                creator
+            }
+            owners {
+                _id
+            }
+            admins {
+                _id
+            }
+        }
+    }
+`;
+
+export const CREATE_LISTING = gql`
+    mutation CreateListing($title: String!, $groupName: String!, $description: String, $value: Int) {
+        createListing(title: $title, groupName: $groupName, description: $description, value: $value) {
+        title
+        description
+        value
+        creator
+        }
+    }
+`;

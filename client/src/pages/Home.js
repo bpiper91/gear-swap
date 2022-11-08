3afecb270aab75d9cb3e57740dc9a6bdb50088d4
bpiper91 <bpiper91@gmail.com>
2022-11-08 13:07:52 -0500

    removed unnecessary code from Home page after GroupList update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import GroupList from '../components/GroupList';
-// import ListingList from '../components/ListingList';
-import { useQuery } from "@apollo/client";
-import { QUERY_GROUPS_PUBLIC, QUERY_LISTINGS_DISPLAY } from "../utils/queries";
 
 const Home = () => {
-  // use useQuery hook to make query request
-  // const { data: groupData } = useQuery(QUERY_GROUPS_PUBLIC);
-  // const { listingsData } = useQuery(QUERY_LISTINGS_DISPLAY)
-  // const groupList = groupData?.groupsPublic || []
-  // const listings = listingsData?.listingsDisplay || []
   return (
     <>
       <main>
@@ -18,9 +10,6 @@ const Home = () => {
             <div className='col-5'>
               <GroupList />
             </div>
-            {/* <div className='col-5'>
-              <ListingList listings={listings}/>
-            </div> */}
           </div>
         </div>
       </main>
